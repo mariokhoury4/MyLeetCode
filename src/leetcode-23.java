13de8c278d7cde09a14bc37e99d9389428fd4858
Mario khoury <mariokhoury4@outlook.com>
2025-07-16 20:58:22 +0200

    Leetcode problem 23

--- src/leetcode-23.java
+++ src/leetcode-23.java
@@ -34,6 +34,48 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        // 1. Initialize a temporary node to build the merged list.
+        ListNode tempNode = new ListNode(0);
 
+        // 2. Initialize a result node to keep track of the head of the merged list.
+        ListNode resultNode = tempNode;
+
+        while (true) {
+            int smallestIndex = findSmallest(lists);
+            if (smallestIndex == -1) {
+                break;
+            }
+            tempNode.next = lists[smallestIndex];
+            tempNode = tempNode.next;
+            lists[smallestIndex] = lists[smallestIndex].next;
+        }
+
+        return resultNode.next;
+    }
+
+    /**
+     * Find the smallest node among the heads of the k linked lists.
+     * @param lists array of linked lists
+     * @return the smallest node
+     */
+    private int findSmallest(ListNode[] lists) {
+        ListNode smallestNode = null;
+        int smallestIndex = -1;
+
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] == null) {
+                continue;
+            }
+            if (smallestNode == null) {
+                smallestNode = lists[i];
+                smallestIndex = i;
+            } else {
+                if (lists[i].val < smallestNode.val) {
+                    smallestNode = lists[i];
+                    smallestIndex = i;
+                }
+            }
+        }
+        return smallestIndex;
     }
 }
