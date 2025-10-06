648cf116866df5ecfaaee7ef2f17df7e4e3779b0
Mario khoury <mariokhoury4@outlook.com>
2025-10-06 19:16:59 +0200

    Leetcode-56

---
+++ src/leetcode-56.java
@@ -0,0 +1,54 @@
+/**
+ * 56. Merge Intervals
+ * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * Output: [[1,6],[8,10],[15,18]]
+ * Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+ * Example 2:
+ *
+ * Input: intervals = [[1,4],[4,5]]
+ * Output: [[1,5]]
+ * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ * Example 3:
+ *
+ * Input: intervals = [[4,7],[1,4]]
+ * Output: [[1,7]]
+ * Explanation: Intervals [1,4] and [4,7] are considered overlapping.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= intervals.length <= 104
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 104
+ */
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length <= 1) {
+            return intervals;
+        }
+
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> merged = new ArrayList<>();
+        int[] cur = intervals[0].clone(); // [start, end]
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] next = intervals[i];
+            if (next[0] > cur[1]) {
+                merged.add(cur);
+                cur = next.clone();
+            } else {
+                cur[1] = Math.max(cur[1], next[1]);
+            }
+        }
+
+        merged.add(cur);
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
