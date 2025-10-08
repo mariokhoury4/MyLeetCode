025f0cd2594098cbde74cf73876b8b8727e197fd
Mario khoury <mariokhoury4@outlook.com>
2025-10-08 19:37:11 +0200

    Leetcode-74

---
+++ src/leetcode-74.java
@@ -0,0 +1,56 @@
+/**
+ * 74. Search a 2D Matrix
+ * You are given an m x n integer matrix matrix with the following two properties:
+ *
+ * Each row is sorted in non-decreasing order.
+ * The first integer of each row is greater than the last integer of the previous row.
+ * Given an integer target, return true if target is in matrix or false otherwise.
+ *
+ * You must write a solution in O(log(m * n)) time complexity.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+ * Output: true
+ * Example 2:
+ *
+ *
+ * Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+ * Output: false
+ *
+ *
+ * Constraints:
+ *
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 100
+ * -104 <= matrix[i][j], target <= 104
+ */
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int left = 0;
+        int right = (m * n) - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int i = mid / n;
+            int j = mid % n;
+
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                right = mid - 1;
+            } else {
+                left = left + 1;
+            }
+        }
+        return false;
+    }
+}
